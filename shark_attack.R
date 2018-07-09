e7be08c8b87d2524f96dae0954b2aa563735b44c
Tavo <gariosp@unal.edu.co>
2018-07-08 23:55:09 -0300

    Transforming variable sex, adding comments to transformations

--- shark_attack.R
+++ shark_attack.R
@@ -7,11 +7,22 @@ sharksData <- read.csv(file="Shark_Attack_Data.csv", header=TRUE, sep=",")
 glimpse(sharksData)
 
 
-
+#Transformación Variable Case.Number
 sharksData$Case.Number <- gsub(".R", "", sharksData$Case.Number)
 sharksData$Case.Number <- gsub(".a", "", sharksData$Case.Number)
 sharksData$Case.Number <- gsub(".b", "", sharksData$Case.Number)
 sharksData$Case.Number <- gsub(".c", "", sharksData$Case.Number)
 sharksData$Case.Number <- gsub(".d", "", sharksData$Case.Number)
 
+#Transformación Variable Date
 sharksData$Date <- gsub("Reported ", "", sharksData$Date)
+
+#Transformación Variable sex
+sharksData$Sex <- gsub("lli ", "", sharksData$Sex)
+sharksData$Sex <- gsub("N", "", sharksData$Sex)
+sharksData$Sex <- gsub(".", "", sharksData$Sex)
+sharksData$Sex <- gsub(",", "", sharksData$Sex)
+
+
+
+
