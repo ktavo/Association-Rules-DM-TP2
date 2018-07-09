6693a0963d62c860440f8f9649bcf069dd7f3dfb
Tavo <gariosp@unal.edu.co>
2018-07-08 21:24:59 -0300

    Cleaning Case.Number and Date variables

--- shark_attack.R
+++ shark_attack.R
@@ -5,3 +5,13 @@ setwd("E:/UBA/Data Mining/TP2")
 
 sharksData <- read.csv(file="Shark_Attack_Data.csv", header=TRUE, sep=",")
 glimpse(sharksData)
+
+
+
+sharksData$Case.Number <- gsub(".R", "", sharksData$Case.Number)
+sharksData$Case.Number <- gsub(".a", "", sharksData$Case.Number)
+sharksData$Case.Number <- gsub(".b", "", sharksData$Case.Number)
+sharksData$Case.Number <- gsub(".c", "", sharksData$Case.Number)
+sharksData$Case.Number <- gsub(".d", "", sharksData$Case.Number)
+
+sharksData$Date <- gsub("Reported ", "", sharksData$Date)
